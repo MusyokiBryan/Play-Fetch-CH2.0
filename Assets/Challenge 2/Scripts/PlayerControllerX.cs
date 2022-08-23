b239f7d07ab7c00454a731558e5270fa0b3028c1
Brian Musyoki <musyokibr@gmail.com>
2022-08-23 10:25:52 +0300

    Add random spawn Interval & Prevent user from spamming input
    This commit ensures that during game play a player has to space and time spawning dogs to grab a ball rather than spam it and spawn infinite dogs which would ensure no balls ever hit the ground. Also the balls will no longer spawn at a fixed time interval and they will spawn at a random times.

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float downTime = 0;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && downTime <= 0)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            downTime = 0.85f;
         }
+        else if (downTime >= 0)
+        {
+            downTime = downTime - Time.deltaTime;
+        }
+
     }
 }
--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -10,25 +10,28 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXRight = 7;
     private float spawnPositionY = 30;
 
-    private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float startDelay = 1.5f;
+    private float spawnInterval = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
-        // Invoke("SpawnRandomBall", Random.Range(3f, 6f));
+        SpawnRandomBall();
+        
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+         // Generate random ball index and random spawn position
         int ballIndex = Random.Range(0, ballPrefabs.Length);
-        // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPositionY, 0);
 
         // instantiate ball at random spawn location
         Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+
+        //balls now spawn at random intervals
+        Invoke("SpawnRandomBall", Random.Range(startDelay, spawnInterval));
     }
 
 }
